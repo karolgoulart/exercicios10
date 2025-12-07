b4bae5363325a22922a4c9c84fc43d6c9536afff
Anne Karoline Goulart <annekgoulart@gmail.com>
2025-12-07 14:38:31 -0300

    Implement insert function for sorted linked list

---
+++ aloc6.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Caixa* insere (struct Caixa* lista, int valor){
+    struct Caixa* novo= malloc(sizeof(struct Caixa));
+    novo->valor= valor;
+    novo->prox= NULL;
+    
+    //inserir no inicio
+    if(lista==NULL || valor < lista->valor){
+        novo->prox=lista;
+        return novo; //nova cabeca
+    }
+    
+    //inserio no meio ou final
+    struct Caixa* atual= lista;
+    while(atual->prox!=NULL && atual->prox->valor < valor){
+        atual= atual->prox;
+    }
+    novo->prox= atual->prox;
+    atual->prox= novo;
+    
+    return lista;
+    
+}
+
