c2f17280d216d377210c49250033cedb771ff890
Anne Karoline Goulart <annekgoulart@gmail.com>
2025-12-07 13:45:23 -0300

    Adiciona estrutura de lista encadeada e função 'contem'
    Implementa a estrutura de lista encadeada e a função 'contem' para verificar se um valor está presente.

---
+++ aloc4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Caixa {
+    int valor;
+    struct Caixa* prox;
+};
+
+int contem (struct Caixa* lista, int valor){
+  struct Caixa* atual= lista;
+
+  while(atual != NULL){
+    if(atual->valor != valor){
+    return 0;
+    }
+    atual= atual->prox;
+  }
+  return 0;
+}
